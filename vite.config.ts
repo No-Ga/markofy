f6caa669daa29b7d2a94cf7fc0d997dea481e20a
Filip Hallberg <filip.hallberg@nordiskagalleriet.com>
2025-11-21 11:50:38 +0100

    adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import marko from "@marko/run/vite";
 export default defineConfig({
   plugins: [marko()],
   build: {
-    assetsInlineLimit: 0, // This is currently a work around for loading the favicon since datauri does not work.
+    minify: true,
+    emptyOutDir: false,
+    assetsInlineLimit: 0,
     rollupOptions: {
       output: {
-        // Output ESM for the server build also.
-        // Remove when https://github.com/vitejs/vite/issues/2152 is resolved.
         format: "es",
       },
     },
+    ssr: process.env.NODE_ENV === "production",
   },
 });
